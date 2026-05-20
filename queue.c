613816306f90205cbb1857043990c13584c9c9c7
jiyun579 <jiyun579@example.com>
2026-05-20 19:27:54 +0900

    modify: queue.c

--- queue.c
+++ queue.c
@@ -58,16 +58,14 @@ Process* Dequeue_Ready_SJF(Process* ready_queue[], int* ready_count) {
     int best_idx = 0;
 
     for (int i = 1; i < *ready_count; i++) {
-        // remaining_cpu
+        // 1. 남은 cpu 시간이 더 짧은 것 우선
         if (ready_queue[i]->remaining_cpu < ready_queue[best_idx]->remaining_cpu) best_idx = i;
+        // 2. 남은 cpu 시간이 같다면 먼저 도착한 것 우선
         else if (ready_queue[i]->remaining_cpu == ready_queue[best_idx]->remaining_cpu) {
-            // arrival_time
-            if (ready_queue[i]->arrival_time < ready_queue[best_idx]->arrival_time) {
-                best_idx = i;
-            }
-            // priority
+            if (ready_queue[i]->arrival_time < ready_queue[best_idx]->arrival_time) best_idx = i;
+            // 3. 도착한 시간이 같다면 pid가 작은 것 우선
             else if (ready_queue[i]->arrival_time == ready_queue[best_idx]->arrival_time) {
-                if (ready_queue[i]->priority > ready_queue[best_idx]->priority) best_idx = i;
+                if (ready_queue[i]->pid < ready_queue[best_idx]->pid) best_idx = i;
             }
         }
     }
@@ -82,11 +80,15 @@ Process* Dequeue_Ready_Priority(Process* ready_queue[], int* ready_count) {
     int best_idx = 0;
 
     for (int i = 1; i < *ready_count; i++) {
-        // priority
+        // 1. 더 높은 우선순위 우선
         if (ready_queue[i]->priority > ready_queue[best_idx]->priority) best_idx = i;
-        // arrival_time
+        // 2. 우선순위가 같다면 먼저 도착한 것 우선
         else if (ready_queue[i]->priority == ready_queue[best_idx]->priority) {
             if (ready_queue[i]->arrival_time < ready_queue[best_idx]->arrival_time) best_idx = i;
+            // 3. 도착한 시간이 같다면 pid가 작은 것 우선
+            else if (ready_queue[i]->arrival_time == ready_queue[best_idx]->arrival_time) {
+                if (ready_queue[i]->pid < ready_queue[best_idx]->pid) best_idx = i;
+            }
         }
     }
 
